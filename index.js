be0b7ade681d8b7b0ae3183bd0267a2af7d46fb1
JazzPants <leighton.l14@gmail.com>
2021-11-25 13:30:48 +1100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+//explorer.exe index.html
+//or go to "Live server"
+
+/*
+The methods and properties that the DOM provides via its objects 
+is called the DOM's "Application Programming Interface," or "API." 
+It's just a programming word that you're likely to see online. 
+But it just means "the things that these objects know how to do."
+*/
